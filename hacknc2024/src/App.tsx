35cf8ed8c161178bed91a680a0b5dc0c15a96e04
ErikSvanes <116416661+ErikSvanes@users.noreply.github.com>
2024-11-02 18:20:05 -0400

    Revert "bringing it in"

--- hacknc2024/src/App.tsx
+++ hacknc2024/src/App.tsx
@@ -1,34 +1,16 @@
-// App.js
-import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
-import Profile from './Profile';
-import Settings from './Settings';
-import About from './About';
-import Event from './Event';
-import { Box, Flex } from '@chakra-ui/react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css'
+import Home from './Home'
+import Navbar from './Navbar'
+import {Box} from '@chakra-ui/react'
 
 function App() {
+
     return (
-        <Router>
-            <Box position="fixed" top="0" left="0" width="100vw">
-                <Navbar />
-            </Box>
-            <Flex alignContent="center">
-              <Box mt={7} width="90vw"> {/* Adjust margin to avoid overlap with Navbar */}
-                  <Routes>
-                      <Route path="/" element={<Navigate to="/home" replace />} />
-                      <Route path="/home" element={<Home />} />
-                      <Route path="/profile" element={<Profile />} />
-                      <Route path="/settings" element={<Settings />} />
-                      <Route path="/about" element={<About />} />
-                      {/* Add other routes as needed */}
-                  </Routes>
-              </Box>
-            </Flex>
-        </Router>
-    );
+        <Box position="fixed" top="0" left="0" width="100vw">
+            <Navbar/>
+            <Home/>
+        </Box>
+    )
 }
 
-export default App;
+export default App
